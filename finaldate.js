4e42da9bb78a9f41e009b1e06a1d53322fb20efa
Your Name <you@example.com>
2017-09-28 18:08:38 +0400

    sadasd

--- finaldate.js
+++ finaldate.js
@@ -7,6 +7,21 @@ var myInterface = readline.createInterface({
 });
 myInterface.on('line', function (line) {
   
-  
+  var infouser=JSON.parse(line);
+  var url=infouser.repos+"?client_id=2b13a65da98f9faa066c&client_secret=4e3f6e03bf898cdd5a2ff2c6ef65b233368b3677";
+  var options={
+                     url:url,
+                     headers:{
+                        'User-Agent':'request'
+                      }
+              };
+  request(options,function(err,response,json){
+      var reqinfo=JSON.parse(json);
+      console.log(reqinfo);
+      forEach(reqinfo,function(link1,index1,arr1){
+          console.log(1);
+          
+      });
+  });
   
 });
